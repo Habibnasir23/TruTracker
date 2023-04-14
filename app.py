e3a2aa22a2062e7bb83d450ab98acfbadb349aaf
Thaddeus-Bielecki <twb5147@truman.edu>
2023-04-13 22:40:24 -0500

    Created the back button on the page inside the map.html

--- app.py
+++ app.py
@@ -306,6 +306,61 @@ def homeMap(lat_src,long_src,lat_dist,long_dist):
     m.get_root().height = "600px"
     iframe = m.get_root()._repr_html_()
 
+    m.get_root().html.add_child(folium.Element("""
+    <div style="position: fixed; 
+     top: 50px; left: 70px; width: 163px; height: 100px;
+     background-color:white; border-radius: 5px; border:2px solid grey;z-index: 900;">
+    <h5>Route new destination<h5>
+    <button class="button-9" role="button">Back</button>
+
+<style>
+.button-9 {
+  appearance: button;
+  backface-visibility: hidden;
+  background-color: #405cf5;
+  border-radius: 6px;
+  border-width: 0;
+  box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset,rgba(50, 50, 93, .1) 0 2px 5px 0,rgba(0, 0, 0, .07) 0 1px 1px 0;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  font-family: -apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif;
+  font-size: 100%;
+  height: 44px;
+  line-height: 1.15;
+  outline: none;
+  overflow: hidden;
+  padding: 0 25px;
+  position: absolute;
+  text-align: center;
+  text-transform: none;
+  transform: translateZ(0);
+  transition: all .2s,box-shadow .08s ease-in;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: 50%;
+  margin-left: 10px;
+}
+
+.button-9:hover{
+  background: #2c52ed;
+}
+
+.button-9:disabled {
+  cursor: default;
+}
+
+.button-9:focus {
+  box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset, rgba(50, 50, 93, .2) 0 6px 15px 0, rgba(0, 0, 0, .1) 0 2px 2px 0, rgba(50, 151, 211, .3) 0 0 0 4px;
+}
+5{
+margin-left: 5px;
+text-align: center;
+}
+</style>
+    </div>
+    """))
 
     m.save('templates/map.html')
     return render_template('map.html', iframe=iframe)
